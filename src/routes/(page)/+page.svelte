eb38a4349cb0ec5fb374b416d9602e1adb064f50
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-22 20:18:22 -0700

    initial build

---
+++ src/routes/(page)/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { onMount } from 'svelte';
+	let Geometry;
+
+	onMount(async () => {
+		const module = await import('$lib/components/geometry/geometry.svelte');
+		Geometry = module.default;
+	});
+</script>
+
+<main>
+	<div class="sidebar">
+		<p>SiiiGNAL: Topographical Connectivity</p>
+		<p>version: 0.0.1</p>
+		<br />
+		<p>globe view</p>
+		<p>field</p>
+	</div>
+	<svelte:component this={Geometry} />
+</main>
+
+<style>
+	main {
+		position: relative;
+		height: calc(100vh - 51px);
+		overflow: hidden;
+		display: flex;
+	}
+
+	.sidebar {
+		width: calc(100vw / 4);
+		border-right: solid 1px var(--white-50);
+
+		padding: 20px;
+	}
+
+	.geometry {
+		width: calc(100vw / 4 * 3);
+	}
+</style>
