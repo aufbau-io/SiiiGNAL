f41abf5bea2cec476524c94dd938ebd3be12f471
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-22 20:43:10 -0700

    start

--- src/routes/(page)/+page.svelte
+++ src/routes/(page)/+page.svelte
@@ -12,15 +12,15 @@
 	<div class="sidebar">
 		<h4>SiiiGNAL</h4>
 		<h6>version: 0.0.1</h6>
+		<h6>instructions</h6>
 		<br />
-		<h6>globe view</h6>
+		<h6>globe view [ <b>on</b> ]</h6>
 		<h6>field view</h6>
 		<br />
-		<h6>rivers: <b>on</b> / off</h6>
-		<h6>connections: <b>on</b> / off</h6>
+		<h6>rivers: [ <b>on</b> / off ]</h6>
+		<h6>connections: [ <b>on</b> / off ]</h6>
 		<br />
-		<h6>instructions</h6>
-		<h6><b>start game</b></h6>
+		<h6>START GAME</h6>
 	</div>
 	<svelte:component this={Geometry} />
 </main>
@@ -34,6 +34,8 @@
 	}
 
 	.sidebar {
+		display: flex;
+		flex-flow: column nowrap;
 		width: calc(100vw / 4);
 		border-right: solid 1px var(--white-50);
 
